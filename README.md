4b0691c59ef3d56de3138570d331d297ce7cc327
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2021-03-25 23:18:42 +0530

    Add Push Script
    .github/workflows/actions.yml
    README.md
    push.sh

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 Daily GitHub Actions Runner which pushes IP To ASN JSON to GitLab. 
 
 It uses [ASN Intelligence](https://github.com/pavel-odintsov/asn_intelligence) to parse full dumps of BGP tables and create a JSON with IPs to ASNs mapping.
+
+More details at: https://github.com/pavel-odintsov/asn_intelligence
---
+++ push.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Script to Push Change(s) to GitLab
+# Author: Jugal Kishore
+
+set -e
+
+# Colors
+NC="\033[0m"
+RED="\033[0;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+
+# Variable(s)
+GL_REF="gitlab.com/crazyuploader/ip_asn_json.git"
+
+# Check if GITLAB_TOKEN Environment Variable is available
+if [[ -z "${GITLAB_TOKEN}" ]]; then
+    echo -e "${YELLOW}'GITLAB_TOKEN' ${RED}Environment Variable not found, exiting...${NC}"
+    exit 1
+fi
+
+# Clone GitLab Repository
+git clone https://"${GL_REF}" --branch master ~/ip_asn_json
+
+cp /tmp/prefix_asn_mapping_v4.json ~/ip_asn_json
+cp /tmp/prefix_asn_mapping_v6.json ~/ip_asn_json
+
+# Setting Git Identity
+git config --global user.email "4677226-crazyuploader@users.noreply.gitlab.com"
+git config --global user.name "Jugal Kishore"
+
+cd ~/ip_asn_json
+if [[ -z $(git status --porcelain) ]]; then
+    echo -e "${GREEN}Nothing to Commit${NC}"
+else
+    git add .
+    git commit -m "GitHub Actions Daily Runner"
+    git push https://crazyuploader:"${GITLAB_TOKEN}"@"${GL_REF}" HEAD:master
+    echo ""
+    echo -e "${GREEN}Change(s) Pushed to: ${YELLOW}https://"${GL_REF}"${NC}"
+fi
